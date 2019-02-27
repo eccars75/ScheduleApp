116758c692227baab799b8ebfb830bc1e639341b
eccars75 <eccars75@thomasmore.edu>
2019-02-27 17:52:31 -0500

    added new migration

--- ScheduleApp/Migrations/201902272251530_mig3.Designer.cs
+++ ScheduleApp/Migrations/201902272251530_mig3.Designer.cs
@@ -7,13 +7,13 @@ namespace ScheduleApp.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class mig2 : IMigrationMetadata
+    public sealed partial class mig3 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(mig2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(mig3));
         
         string IMigrationMetadata.Id
         {
-            get { return "201902271754126_mig2"; }
+            get { return "201902272251530_mig3"; }
         }
         
         string IMigrationMetadata.Source
